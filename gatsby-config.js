dc6563fa05c4604b448468dbba86e4a8d7df3ed2
unknown <vinceoldmark@gmail.com>
2022-03-01 20:26:26 +0000

    upgraded packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: `gatsby-source-contentful`,
       options: {
